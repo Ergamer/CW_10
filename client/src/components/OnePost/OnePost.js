33e3f685c312e758fc64a702a4519e404be4b6c8
Ilgiz <stesfvnlfsnv@mail.ru>
2018-04-21 15:00:40 +0600

    changed_postcreator

--- client/src/components/OnePost/OnePost.js
+++ client/src/components/OnePost/OnePost.js
@@ -8,7 +8,6 @@ class OnePost extends Component {
             <div className="OnePost">
                 <h2>{this.props.author}</h2>
                 <img src={this.props.image} alt=""/>
-                <p>{this.props.date}</p>
                 <button>Read full post</button>
                 <button>Delete</button>
             </div>
--- client/src/containers/PostCreator/PostCreator.js
+++ client/src/containers/PostCreator/PostCreator.js
@@ -1,64 +1,71 @@
 import React, {Component} from 'react';
 import './PostCreator.css';
 import {connect} from "react-redux";
-
+import {fetchPostPost} from "../../store/actions";
 
 class PostCreator extends Component {
-
 
     state = {
         title: '',
         content: '',
-        image: '',
-        date: ''
+        image: ''
+    };
+
+    submitFormHandler = event => {
+        event.preventDefault();
+        const formData = new FormData();
+        Object.keys(this.state).forEach(key => {
+            formData.append(key, this.state[key]);
+        });
+
+        this.props.fetchPostPost(formData);
+    };
+
+    inputChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    fileChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.files[0]
+        });
     };
 
 
-    contactValueChanged = (event) => {
-        event.persist();
-        this.setState({[event.target.name]: event.target.value});
-    };
-
-    render() {
+    render () {
         return (
-            <form className="PostCreator" action="">
-                <div>Title<input
-                    name="name"
-                    value={this.state.title}
-                    onChange={this.contactValueChanged}
-                    type="text"/>
-                </div>
-                <div>Content<textarea
-                    value={this.state.content}
-                    onChange={this.contactValueChanged}
-                    name="content"
-                    type="text"/>
-                </div>
-                <div>Image
-                    <input
-                        value={this.state.image}
-                        onChange={this.contactValueChanged}
-                        name="image"
-                        type="text"/>
-                </div>
-                <button onClick={(e) => this.props.saveContactChanges(e, this.state)}>Save</button>
-            </form>
-        )
+            <div className="PostCreator">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Enter your news"
+                    onChange={this.inputChangeHandler}
+                />
+                <textarea
+                    name="text"
+                    placeholder="Enter your content"
+                    onChange={this.inputChangeHandler}
+                ></textarea>
+                <input type="file"
+                       name="image"
+                       placeholder="Attach your image"
+                       onChange={this.fileChangeHandler}
+                />
+                <button onClick={this.submitFormHandler}>Save</button>
+            </div>
+        );
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        contacts: state.contacts
-    }
-};
-
 const mapDispatchToProps = dispatch => {
     return {
-        saveContactChanges: (e, contacts) => dispatch(saveContactChanges(e, contacts))
+        fetchPostPost: (post) => dispatch(fetchPostPost(post)),
+
     }
 };
 
 
-export default connect (mapStateToProps, mapDispatchToProps) (PostCreator);
+export default connect (null, mapDispatchToProps) (PostCreator);
 
